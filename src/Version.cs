f603ff190063037db9da6e8c7ac3f48f2fa60784
derkalle4 <kalle@kandru.de>
2026-07-06 03:43:34 +0200

    bump version to 26.7.6

--- src/Version.cs
+++ src/Version.cs
@@ -6,6 +6,6 @@ namespace MimesisPlayerEnhancement
     /// </summary>
     public static class VersionInfo
     {
-        public const string ModuleVersion = "26.7.5";
+        public const string ModuleVersion = "26.7.6";
     }
 }
