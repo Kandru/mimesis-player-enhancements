8e199c327936a775bf53ee88535a48fa4abcc420
derkalle4 <kalle@kandru.de>
2026-07-16 23:55:58 +0200

    bump version to 26.7.13

--- src/Version.cs
+++ src/Version.cs
@@ -6,7 +6,7 @@ namespace MimesisPlayerEnhancement
     /// </summary>
     public static class VersionInfo
     {
-        public const string ModuleVersion = "26.7.12";
+        public const string ModuleVersion = "26.7.13";
 
         /// <summary>MIMESIS game version this release was built and tested against.</summary>
         public const string GameVersion = "0.3.0";
